a74b003459d8f74ba06653af79f2174425041168
Ko6ra <ko6ra383@mail.ru>
2022-07-01 21:42:52 +0700

    Добавьте файлы проекта.

---
+++ FIleRncryptor.WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FIleRncryptor.WPF.ViewModels.Base;
+
+namespace FIleRncryptor.WPF.ViewModels
+{
+    internal class MainViewModel : BaseViewModel
+    {
+        #region Properties
+        #region Title
+        private string _Title = "Encryptor";
+        public string Title { get => _Title; set => Set(ref _Title, value); }
+        #endregion  
+        #endregion
+    }
+}
