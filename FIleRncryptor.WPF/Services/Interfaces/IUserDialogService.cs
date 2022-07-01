9c7f87c753c72abfdad82da0dd50b79d0d5a63b1
Ko6ra <ko6ra383@mail.ru>
2022-07-02 02:49:36 +0700

    Заготовка окна

--- FIleRncryptor.WPF/Services/Interfaces/IUserDialogService.cs
+++ FIleRncryptor.WPF/Services/Interfaces/IUserDialogService.cs
@@ -1,6 +1,10 @@
-﻿namespace FIleRncryptor.WPF.Services.Interfaces
+﻿using System.Collections.Generic;
+
+namespace FIleRncryptor.WPF.Services.Interfaces
 {
     internal interface IUserDialogService
     {
+        bool OpenFile(string Title, out string SelectedFile, string Filter = "Все файлы (*.*)|*.*");
+        bool OpenFiles(string Title, out IEnumerable<string> SelectedFiles, string Filter = "Все файлы (*.*)|*.*");
     }
 }
